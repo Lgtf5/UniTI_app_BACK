d124ee9bf9c8249052dabd67789f8f1fadd6041a
Lidia Glez-Tablas <lgtablasf5@gmail.com>
2025-03-16 03:13:01 +0100

    init project

---
+++ src/main/java/dev/lidia/UniTI/UniTiApplication.java
@@ -0,0 +1,13 @@
+package dev.lidia.UniTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniTiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lidia/UniTI/UniTiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lidia.UniTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
