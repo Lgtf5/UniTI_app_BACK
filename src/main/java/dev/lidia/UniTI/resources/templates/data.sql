939e110b1b71c8a3e2dbbb94da29b8805d6d679b
Lidia Glez-Tablas <lgtablasf5@gmail.com>
2025-03-17 09:49:12 +0100

    add data.sql

---
+++ src/main/java/dev/lidia/UniTI/resources/templates/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (id; username; password) VALUES ('user1', 'Sara16', 'ps123');
+INSERT INTO users (id; username; password) VALUES ('user2', 'marioD', 'ps234');
+INSERT INTO users (id; username; password) VALUES ('user3', 'laia3', 'ps345');
+INSERT INTO users (id; username; password) VALUES ('user4', 'meritxell_m', 'ps456');
+INSERT INTO resources (id, category, link, description) VALUES (1, 'Frontend', 'https://sass-lang.com/', 'Página del propio procesador de CSS (Sass) que te hace más fácil y eficiente el desarrollo' );
+INSERT INTO resources (id, category, link, description) VALUES (2, 'Backend', '  https://www.datacamp.com/es/doc/java/polymorphism', 'Página dónde explican el Polimorfismo Java con tipos, ejemplos y buenas prácticas' );
+INSERT INTO resources (id, category, link, description) VALUES (3, 'Frontend', '  https://css-tricks.com/snippets/css/complete-guide-grid/', 'Guía completísima sobre CSS GRID y con ejemplos' );
+INSERT INTO resources (id, category, link, description) VALUES (4, 'Testing', 'https://web.dev/learn/testing/get-started/what-testing-is?hl=es-419', 'Página donde encuentras sección de testing con ejemplos y pruebas a librerías' );
