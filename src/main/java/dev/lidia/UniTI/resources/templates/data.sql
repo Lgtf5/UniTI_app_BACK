085b4ff46884c4fc378f503b7aa15dc7aeac78f3
Lidia Glez-Tablas <lgtablasf5@gmail.com>
2025-03-17 09:51:37 +0100

    fix sql

--- src/main/java/dev/lidia/UniTI/resources/templates/data.sql
+++ src/main/java/dev/lidia/UniTI/resources/templates/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO users (id; username; password) VALUES ('user1', 'Sara16', 'ps123');
-INSERT INTO users (id; username; password) VALUES ('user2', 'marioD', 'ps234');
-INSERT INTO users (id; username; password) VALUES ('user3', 'laia3', 'ps345');
-INSERT INTO users (id; username; password) VALUES ('user4', 'meritxell_m', 'ps456');
+INSERT INTO users (id; username; password) VALUES (1, 'Sara16', 'ps123');
+INSERT INTO users (id; username; password) VALUES (2, 'marioD', 'ps234');
+INSERT INTO users (id; username; password) VALUES (3, 'laia3', 'ps345');
+INSERT INTO users (id; username; password) VALUES (4, 'meritxell_m', 'ps456');
 INSERT INTO resources (id, category, link, description) VALUES (1, 'Frontend', 'https://sass-lang.com/', 'Página del propio procesador de CSS (Sass) que te hace más fácil y eficiente el desarrollo' );
 INSERT INTO resources (id, category, link, description) VALUES (2, 'Backend', '  https://www.datacamp.com/es/doc/java/polymorphism', 'Página dónde explican el Polimorfismo Java con tipos, ejemplos y buenas prácticas' );
 INSERT INTO resources (id, category, link, description) VALUES (3, 'Frontend', '  https://css-tricks.com/snippets/css/complete-guide-grid/', 'Guía completísima sobre CSS GRID y con ejemplos' );
